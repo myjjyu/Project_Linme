b83d62f04957c5336ab5171b63f9953b82befe32
jinju <tlawlswn28@naver.com>
2024-10-25 13:19:50 +0900

    Initial Commit Linme Start

---
+++ linme/src/main/java/kr/project/linme/LinmeApplication.java
@@ -0,0 +1,13 @@
+package kr.project.linme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinmeApplication.class, args);
+	}
+
+}
