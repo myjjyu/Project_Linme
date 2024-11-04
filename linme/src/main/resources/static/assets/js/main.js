a027ddcc1152af9f482b816cd92521558a849226
Gilju Song <rlfwn@gmail.com>
2024-11-04 23:12:32 +0900

    Design: 메인페이지UI 구현중
    메인영역- 반복배열 구현완료 추후에 DB정보 수정필요
    전체상품 영역 구현 진행중

---
+++ linme/src/main/resources/static/assets/js/main.js
@@ -0,0 +1,217 @@
+const contentData = [
+  {
+    menu: "내 몸의 활력을 위한 멀티 비타민",
+    title: "💪비타민 충전타임💪",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "유산균으로 장 편한 하루를 보내보세요",
+    title: "💊온 가족 장 건강 케어💊",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "건강한 혈행을 위해 필요한 영양소",
+    title: "✔️대체불가 필수 지방산 오메가-3✔️",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "성장기 우리 아이 맛있게 영양 보충 해주세요.",
+    title: "👧우리 아이를 위한 영양 보충제👦",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "내 몸의 기둥을 지켜라!",
+    title: "🦴뼈 튼튼, 관절 튼튼👌",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "환절기 대비 영양제 모음",
+    title: "😷환절기 미리 준비하기😷",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "뇌의 건강이 곧 심신의 건강!",
+    title: "✨뇌를 깨워주세요✨",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "혈당, 혈관, 혈행. 피도 관리가 필요해요",
+    title: "🩸혈(血)의 중요성🩸",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "유산균으로 장 편한 하루를 보내보세요",
+    title: "💊온 가족 장 건강 케어💊",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "눈 건강을 위해 루테인 상품을 추천드려요!",
+    title: "👀피로한 눈을 위한 솔루션👀",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "건강한 아름다움을 위한 이너뷰티 추천템",
+    title: "🙌오늘이 가장 젊은 날🙌",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+  {
+    menu: "남성 활력 증진에 필요한 영양제 모음",
+    title: "남성 영양제 추천📣",
+    products: [
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+      { image: "../../static/assets/img/main/네츄럴메이드 멀티비타민.jpg", productName: "네추럴메이드 멀티비타민 앤 미네랄", discountRate: "45", originalPrice: "36,000", salePrice: "19,800", shippingInfo: "무료배송" },
+    ]
+  },
+];
+
+const productContainer = document.getElementById("productContainer");
+
+// 반복문으로 contents 생성
+contentData.forEach((content) => {
+  const contentsDiv = document.createElement("div");
+  contentsDiv.classList.add("contents");
+
+  // headerMenu 추가
+  const headerMenu = document.createElement("div");
+  headerMenu.classList.add("headerMenu");
+  headerMenu.textContent = content.menu;
+
+  // headerMenutitle 추가
+  const headerMenutitle = document.createElement("div");
+  headerMenutitle.classList.add("headerMenutitle");
+  headerMenutitle.innerHTML = content.title;
+
+  const buttonLink = document.createElement("a");
+  buttonLink.href = "otherPage.html";
+  const button = document.createElement("button");
+  button.type = "button";
+  const img = document.createElement("img");
+  img.src = "../../static/assets/img/main/arrow_showMore.png";
+  img.alt = "arrow_showMore";
+  button.appendChild(img);
+  buttonLink.appendChild(button);
+  headerMenutitle.appendChild(buttonLink);
+
+  contentsDiv.appendChild(headerMenu);
+  contentsDiv.appendChild(headerMenutitle);
+
+  // headerMenutitleimg 생성
+  const headerMenutitleimg = document.createElement("div");
+  headerMenutitleimg.classList.add("headerMenutitleimg");
+
+  // 각 제품 추가
+  content.products.forEach((product) => {
+    const boximg = document.createElement("div");
+    boximg.classList.add("boximg");
+
+    const link = document.createElement("a");
+    link.href = "./상세페이지"; // 상세 페이지로의 링크 설정
+
+    const productImg = document.createElement("img");
+    productImg.src = product.image; // 이미지 경로
+    productImg.alt = product.productName; // 제품명으로 대체
+    productImg.classList.add("imgbox");
+
+    const productName = document.createElement("p");
+    productName.classList.add("productName");
+    productName.textContent = product.productName; // 제품 이름으로 수정
+
+    const priceContainer = document.createElement("div"); // 할인율이랑 원가랑 한줄에 묶기
+    priceContainer.classList.add("priceContainer");
+
+    const discountRate = document.createElement("p");
+    discountRate.classList.add("discountRate");
+    discountRate.textContent = `${product.discountRate}%`; // 할인율 표시
+  
+    const originalPrice = document.createElement("p");
+    originalPrice.classList.add("originalPrice");
+    originalPrice.textContent = `${product.originalPrice}원`; // 원가 표시
+
+    const salePrice = document.createElement("p");
+    salePrice.classList.add("salePrice");
+    salePrice.textContent = `${product.salePrice}원`; // 판매가 표시
+
+      // 할인가와 원래 가격을 priceContainer에 추가
+  priceContainer.append(discountRate, originalPrice);
+
+    const shippingInfo = document.createElement("p");
+    shippingInfo.classList.add("shippingInfo");
+    shippingInfo.textContent = product.shippingInfo; // 배송 정보
+
+    link.append(productImg, productName,priceContainer, salePrice, shippingInfo);
+    boximg.appendChild(link);
+    headerMenutitleimg.appendChild(boximg);
+  });
+
+  contentsDiv.appendChild(headerMenutitleimg);
+  productContainer.appendChild(contentsDiv);
+});
