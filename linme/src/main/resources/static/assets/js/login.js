bbea8795f6c5b1edec990108923ab6fa1360a317
jiye <cg2522@naver.com>
2024-11-06 00:51:26 +0900

    Design: Login UI 구현 진행중 #2
    로그인  Html / Javascript 작업 진행중

---
+++ linme/src/main/resources/static/assets/js/login.js
@@ -0,0 +1,16 @@
+// 로그인 눈아이콘 //
+document.getElementById("pwShowHide").addEventListener("click", function () {
+    const userPW = document.getElementById("userPW");
+    const passwordIcon = document.getElementById("pwShowHide");
+  
+    // 패스워드가 표시된 상태인지 확인
+    if (userPW.type === "password") {
+      // 패스워드 보이기
+      userPW.type = "text";
+      passwordIcon.src = "../static/assets/img/login/password(2).png";
+    } else {
+      // 패스워드 숨기기
+      userPW.type = "password";
+      passwordIcon.src = "../static/assets/img/login/password.png";
+    }
+  });
