70e3247cc2b1280b91a7730a588875772c4d6217
yujeong <yujeong010817@gmail.com>
2024-11-07 01:46:31 +0900

    feat: 1:1 문의 UI 구현 #25

---
+++ linme/src/main/resources/static/assets/js/myPage/injury.js
@@ -0,0 +1,19 @@
+// 팝업 열기
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.inquiryButton').addEventListener('click', function() {
+        document.getElementById('inquiryPopup').style.display = 'flex';
+    });
+});
+
+// 팝업 닫기
+function closePopup() {
+    document.getElementById('inquiryPopup').style.display = 'none';
+}
+
+// 글자 수 세기
+const textarea = document.getElementById('inquiryContent');
+const charCount = document.querySelector('.char-count');
+
+textarea.addEventListener('input', function() {
+    charCount.textContent = `${textarea.value.length} / 300자`;
+});
