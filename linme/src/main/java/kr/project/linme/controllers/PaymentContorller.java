528d4e47b81111cac951ce57f0e0a8c186206639
jinju <tlawlswn28@naver.com>
2024-11-08 17:40:39 +0900

    Design: 주문/결제 UI 구현중 #32
    배송지 정보 CSS 구현중

--- linme/src/main/java/kr/project/linme/controllers/PaymentContorller.java
+++ linme/src/main/java/kr/project/linme/controllers/PaymentContorller.java
@@ -1,14 +1,19 @@
 package kr.project.linme.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class PaymentContorller {
     @GetMapping("/payment/payment")
-    public String payment() {
+    public String payment(Model model) {
+        model.addAttribute("addressNickname", "주소 닉네임");
+        model.addAttribute("addressName", "회원이름");
+        model.addAttribute("phone", "회원 전화번호");
+        model.addAttribute("address", "회원 주소");
+        
         return "payment/payment";
     }
-    
 }
