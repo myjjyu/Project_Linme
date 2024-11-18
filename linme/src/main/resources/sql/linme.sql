0ed896c3a7da620b01485d25646322417b67d5c8
jinju0323 <tlawlswn28@naver.com>
2024-11-18 22:26:20 +0900

    Fix: Table id 변경에 따른 .sql 파일 재업로드

--- linme/src/main/resources/sql/linme.sql
+++ linme/src/main/resources/sql/linme.sql
@@ -21,11 +21,11 @@ USE `linme` ;
 -- Table `linme`.`brand`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `linme`.`brand` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '브랜드 일련번호',
+  `brand_id` INT NOT NULL AUTO_INCREMENT COMMENT '브랜드 일련번호',
   `brand_name` VARCHAR(100) NOT NULL COMMENT '브랜드명',
   `reg_date` DATETIME NOT NULL COMMENT '등록일시',
   `edit_date` DATETIME NOT NULL COMMENT '변경일시',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`brand_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci
@@ -36,7 +36,7 @@ COMMENT = '브랜드';
 -- Table `linme`.`member`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `linme`.`member` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '회원 일련번호',
+  `member_id` INT NOT NULL AUTO_INCREMENT COMMENT '회원 일련번호',
   `user_id` VARCHAR(255) NOT NULL COMMENT '아이디(이메일)',
   `user_pw` VARCHAR(255) NOT NULL COMMENT '비밀번호',
   `user_name` VARCHAR(100) NOT NULL COMMENT '회원 이름',
@@ -53,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `linme`.`member` (
   `login_date` DATETIME NULL DEFAULT NULL COMMENT '마지막 로그인 일시',
   `reg_date` DATETIME NOT NULL COMMENT '등록일시',
   `edit_date` DATETIME NOT NULL COMMENT '변경일시',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`member_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci
@@ -64,14 +64,14 @@ COMMENT = '회원';
 -- Table `linme`.`category`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `linme`.`category` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '카테고리 일련번호',
+  `category_id` INT NOT NULL AUTO_INCREMENT COMMENT '카테고리 일련번호',
   `category_name` VARCHAR(100) NOT NULL COMMENT '카테고리명',
   `icon_category` VARCHAR(255) NULL DEFAULT NULL COMMENT '아이콘 카테고리',
   `title_category` VARCHAR(255) NULL DEFAULT NULL COMMENT '타이틀 카테고리',
   `header_item` VARCHAR(10) NULL DEFAULT NULL COMMENT '카테고리 헤더 아이템',
   `reg_date` VARCHAR(45) NOT NULL COMMENT '등록일시',
   `edit_date` DATETIME NOT NULL COMMENT '변경일시',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`category_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci
@@ -82,7 +82,7 @@ COMMENT = '카테고리';
 -- Table `linme`.`product`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `linme`.`product` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '상품 일련번호',
+  `product_id` INT NOT NULL AUTO_INCREMENT COMMENT '상품 일련번호',
   `product_name` VARCHAR(255) NOT NULL COMMENT '상품명',
   `brand_id` INT NOT NULL COMMENT '브랜드 일련번호',
   `price` INT NOT NULL COMMENT '정가',
@@ -95,15 +95,15 @@ CREATE TABLE IF NOT EXISTS `linme`.`product` (
   `discount_price` INT NULL DEFAULT NULL COMMENT '할인 금액',
   `reg_date` DATETIME NOT NULL COMMENT '등록일시',
   `edit_date` DATETIME NOT NULL COMMENT '변경일시',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`product_id`),
   INDEX `category_id` (`category_id` ASC) VISIBLE,
   INDEX `product_ibfk_2` (`brand_id` ASC) VISIBLE,
   CONSTRAINT `product_ibfk_1`
     FOREIGN KEY (`category_id`)
-    REFERENCES `linme`.`category` (`id`),
+    REFERENCES `linme`.`category` (`category_id`),
   CONSTRAINT `product_ibfk_2`
     FOREIGN KEY (`brand_id`)
-    REFERENCES `linme`.`brand` (`id`))
+    REFERENCES `linme`.`brand` (`brand_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci
@@ -114,22 +114,22 @@ COMMENT = '상품';
 -- Table `linme`.`cart`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `linme`.`cart` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '장바구니 일련번호',
+  `cart_id` INT NOT NULL AUTO_INCREMENT COMMENT '장바구니 일련번호',
   `product_count` INT NULL DEFAULT NULL COMMENT '선택한 상품 수량',
   `total_price` INT NULL DEFAULT NULL COMMENT '전체 주문금액',
   `member_id` INT NOT NULL COMMENT '회원 일련번호',
   `product_id` INT NOT NULL COMMENT '상품 일련번호',
   `reg_date` DATETIME NOT NULL COMMENT '등록일시',
   `edit_date` DATETIME NOT NULL COMMENT '변경일시',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`cart_id`),
   INDEX `cart_ibfk_1` (`member_id` ASC) VISIBLE,
   INDEX `cart_ibfk_2` (`product_id` ASC) VISIBLE,
   CONSTRAINT `cart_ibfk_1`
     FOREIGN KEY (`member_id`)
-    REFERENCES `linme`.`member` (`id`),
+    REFERENCES `linme`.`member` (`member_id`),
   CONSTRAINT `cart_ibfk_2`
     FOREIGN KEY (`product_id`)
-    REFERENCES `linme`.`product` (`id`))
+    REFERENCES `linme`.`product` (`product_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci
@@ -140,7 +140,7 @@ COMMENT = '장바구니';
 -- Table `linme`.`img`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `linme`.`img` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '이미지 일련번호',
+  `img_id` INT NOT NULL AUTO_INCREMENT COMMENT '이미지 일련번호',
   `product_id` INT NOT NULL COMMENT '상품 일련번호',
   `img_url` VARCHAR(255) NOT NULL COMMENT '이미지 URL',
   `img_path` VARCHAR(255) NOT NULL COMMENT '이미지 경로',
@@ -152,11 +152,11 @@ CREATE TABLE IF NOT EXISTS `linme`.`img` (
   `d_img3` VARCHAR(45) NULL DEFAULT NULL COMMENT '상세 이미지3',
   `reg_date` DATETIME NOT NULL COMMENT '등록일시',
   `edit_date` DATETIME NOT NULL COMMENT '변경일시',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`img_id`),
   INDEX `img_ibfk_1` (`product_id` ASC) VISIBLE,
   CONSTRAINT `img_ibfk_1`
     FOREIGN KEY (`product_id`)
-    REFERENCES `linme`.`product` (`id`))
+    REFERENCES `linme`.`product` (`product_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci
@@ -167,7 +167,7 @@ COMMENT = '이미지';
 -- Table `linme`.`payment`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `linme`.`payment` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '주문/결제 일련번호',
+  `payment_id` INT NOT NULL AUTO_INCREMENT COMMENT '주문/결제 일련번호',
   `order_name` VARCHAR(100) NOT NULL COMMENT '주문자 이름',
   `order_tel` VARCHAR(20) NOT NULL COMMENT '주문자 연락처',
   `addr1` VARCHAR(255) NOT NULL COMMENT '검색된 주소',
@@ -178,7 +178,7 @@ CREATE TABLE IF NOT EXISTS `linme`.`payment` (
   `total_price` INT NOT NULL COMMENT '전체 주문금액',
   `reg_date` DATETIME NOT NULL COMMENT '등록일시',
   `edit_date` DATETIME NOT NULL COMMENT '변경일시',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`payment_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci
@@ -189,7 +189,7 @@ COMMENT = '주문/결제';
 -- Table `linme`.`order_item`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `linme`.`order_item` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '주문 상품 확인 일련번호',
+  `order_item_id` INT NOT NULL AUTO_INCREMENT COMMENT '주문 상품 확인 일련번호',
   `order_bname` VARCHAR(100) NOT NULL COMMENT '주문 상품 브랜드명',
   `order_pname` VARCHAR(255) NOT NULL COMMENT '주문 상품 상품명',
   `order_count` INT NOT NULL COMMENT '주문 상품 확인 수량',
@@ -198,11 +198,11 @@ CREATE TABLE IF NOT EXISTS `linme`.`order_item` (
   `payment_id` INT NOT NULL COMMENT '주문/결제 일련번호',
   `reg_date` DATETIME NOT NULL COMMENT '등록일시',
   `edit_date` DATETIME NOT NULL COMMENT '변경일시',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`order_item_id`),
   INDEX `fk_order_item_payment1_idx` (`payment_id` ASC) VISIBLE,
   CONSTRAINT `fk_order_item_payment1`
     FOREIGN KEY (`payment_id`)
-    REFERENCES `linme`.`payment` (`id`))
+    REFERENCES `linme`.`payment` (`payment_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci
