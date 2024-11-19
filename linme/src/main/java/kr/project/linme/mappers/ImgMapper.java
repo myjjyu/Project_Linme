b5cead300fe60967c254478a2bc3ae18aab76ffb
jinju0323 <tlawlswn28@naver.com>
2024-11-19 18:59:44 +0900

    Feat: ImgMapper SQL 가독성을 위한 줄바꿈 적용 #38

--- linme/src/main/java/kr/project/linme/mappers/ImgMapper.java
+++ linme/src/main/java/kr/project/linme/mappers/ImgMapper.java
@@ -18,60 +18,82 @@ import kr.project.linme.models.Img;
 public interface ImgMapper {
     /**
      * 이미지 저장
-     * @param input
-     * @return
+     * @param input - 저장할 이미지 정보에 대한 모델 객체
+     * @return 입력된 데이터 수
      */
     @Insert("INSERT INTO img (" + 
-            "product_id, img_url, img_path, " + 
-            "img1, img2, img3, " + 
-            "d_img1, d_img2, d_img3, " + 
-            "reg_date, edit_date) " + 
+                "product_id, " + 
+                "img_url, " + 
+                "img_path, " + 
+                "img1, " + 
+                "img2, " + 
+                "img3, " + 
+                "d_img1, " + 
+                "d_img2, " + 
+                "d_img3, " + 
+                "reg_date, " + 
+                "edit_date) " + 
             "VALUES (" + 
-            "#{productId}, #{imgUrl}, #{imgPath}, " + 
-            "#{img1}, #{img2}, #{img3}, " + 
-            "#{dImg1}, #{dImg2}, #{dImg3}, " + 
-            "#{regDate}, #{editDate})")
+                "#{productId}, " + 
+                "#{imgUrl}, " + 
+                "#{imgPath}, " + 
+                "#{img1}, " + 
+                "#{img2}, " + 
+                "#{img3}, " + 
+                "#{dImg1}, " + 
+                "#{dImg2}, " + 
+                "#{dImg3}, " + 
+                "#{regDate}, " + 
+                "#{editDate})")
     @Options(useGeneratedKeys = true, keyProperty = "imgId", keyColumn = "img_id")
     public int insert(Img input);
 
     /**
      * 이미지 수정
-     * @param input
+     * @param input - 이미지 수정에 대한 모델 객체
      * @return
      */
     @Update("UPDATE img SET " + 
-            "product_id = #{productId}, " + 
-            "img_url = #{imgUrl}, " + 
-            "img_path = #{imgPath}, " + 
-            "img1 = #{img1}, " + 
-            "img2 = #{img2}, " + 
-            "img3 = #{img3}, " + 
-            "d_img1 = #{dImg1}, " + 
-            "d_img2 = #{dImg2}, " + 
-            "d_img3 = #{dImg3}, " + 
-            "reg_date = #{regDate}, " + 
-            "edit_date = #{editDate} " + 
+                "product_id = #{productId}, " + 
+                "img_url = #{imgUrl}, " + 
+                "img_path = #{imgPath}, " + 
+                "img1 = #{img1}, " + 
+                "img2 = #{img2}, " + 
+                "img3 = #{img3}, " + 
+                "d_img1 = #{dImg1}, " + 
+                "d_img2 = #{dImg2}, " + 
+                "d_img3 = #{dImg3}, " + 
+                "reg_date = #{regDate}, " + 
+                "edit_date = #{editDate} " + 
             "WHERE img_id = #{imgId}")
     public int update(Img input);
 
     /**
      * 이미지 삭제
-     * @param input
-     * @return
+     * @param input - 이미지 삭제에 대한 모델 객체
+     * @return 삭제된 데이터 수
      */
     @Delete("DELETE FROM img WHERE img_id = #{imgId}")
     public int delete(Img input);
 
     /**
      * 이미지 단일 조회
-     * @param input
-     * @return
+     * @param input - 이미지 조회에 대한 모델 객체
+     * @return 조회한 데이터 수
      */
     @Select("SELECT " + 
-            "img_id, product_id, img_url, img_path, " + 
-            "img1, img2, img3, " + 
-            "d_img1, d_img2, d_img3, " + 
-            "reg_date, edit_date " + 
+            "img_id, " + 
+            "product_id, " + 
+            "img_url, " + 
+            "img_path, " + 
+            "img1, " + 
+            "img2, " + 
+            "img3, " + 
+            "d_img1, " + 
+            "d_img2, " + 
+            "d_img3, " + 
+            "reg_date, " + 
+            "edit_date " + 
             "FROM img " + 
             "WHERE img_id = #{imgId}")
     @Results(id="imgMap", value={
@@ -92,22 +114,30 @@ public interface ImgMapper {
 
     /**
      * 이미지 다중 조회
-     * @param input
-     * @return
+     * @param input - 이미지 조회에 대한 모델 객체
+     * @return 조회한 데이터 수
      */
     @Select("SELECT " + 
-            "img_id, product_id, img_url, img_path, " + 
-            "img1, img2, img3, " + 
-            "d_img1, d_img2, d_img3, " + 
-            "reg_date, edit_date " + 
+                "img_id, " + 
+                "product_id, " + 
+                "img_url, " + 
+                "img_path, " + 
+                "img1, " + 
+                "img2, " + 
+                "img3, " + 
+                "d_img1, " + 
+                "d_img2, " + 
+                "d_img3, " + 
+                "reg_date, " + 
+                "edit_date " + 
             "FROM img")
     @ResultMap("imgMap")
     public List<Img> selectList(Img input);
 
     /**
      * 이미지 수 조회
-     * @param input
-     * @return
+     * @param input 
+     * @return 조회한 데이터 수
      */
     @Select("SELECT COUNT(*) FROM img")
     public int selectCount(Img input);
