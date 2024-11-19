4686177cbf90ebbce1b220ccf3b5fdb479542e8f
jinju0323 <tlawlswn28@naver.com>
2024-11-19 17:49:10 +0900

    Feat: CartMapper SQL 가독성을 위한 줄바꿈 적용 #37

--- linme/src/main/java/kr/project/linme/mappers/CartMapper.java
+++ linme/src/main/java/kr/project/linme/mappers/CartMapper.java
@@ -18,52 +18,62 @@ import kr.project.linme.models.Cart;
 public interface CartMapper {
     /**
      * 장바구니 저장
-     * @param input
-     * @return
+     * @param input 입력할 장바구니 정보에 대한 모델 객체
+     * @return 입력된 데이터 수
      */
     @Insert("INSERT INTO cart (" + 
-            "product_count, total_price, " + 
-            "member_id, product_id, " + 
-            "reg_date, edit_date) " + 
+                "product_count, " + 
+                "total_price, " + 
+                "member_id, " + 
+                "product_id, " + 
+                "reg_date, " + 
+                "edit_date) " + 
             "VALUES (" + 
-            "#{productCount}, #{totalPrice}, " + 
-            "#{memberId}, #{productId}, " + 
-            "#{regDate}, #{editDate})")
+                "#{productCount}, " + 
+                "#{totalPrice}, " + 
+                "#{memberId}, " + 
+                "#{productId}, " + 
+                "#{regDate}, " + 
+                "#{editDate})")
     @Options(useGeneratedKeys = true, keyProperty = "cartId", keyColumn = "cart_id")
     public int insert(Cart input);
 
     /**
      * 장바구니 수정
-     * @param input
-     * @return
+     * @param input - 수정할 장바구니 정보에 대한 모델 객체
+     * @return 수정된 데이터 수
      */
     @Update("UPDATE cart SET " + 
-            "product_count = #{productCount}, " + 
-            "total_price = #{totalPrice}, " + 
-            "member_id = #{memberId}, " + 
-            "product_id = #{productId}, " + 
-            "reg_date = #{regDate}, " + 
-            "edit_date = #{editDate} " + 
+                "product_count = #{productCount}, " + 
+                "total_price = #{totalPrice}, " + 
+                "member_id = #{memberId}, " + 
+                "product_id = #{productId}, " + 
+                "reg_date = #{regDate}, " + 
+                "edit_date = #{editDate} " + 
             "WHERE cart_id = #{cartId}")
     public int update(Cart input);
 
     /**
      * 장바구니 삭제
-     * @param input
-     * @return
+     * @param input - 삭제할 장바구니 정보에 대한 모델 객체
+     * @return 삭제된 데이터 수
      */
     @Delete("DELETE FROM cart WHERE cart_id = #{cartId}")
     public int delete(Cart input);
 
     /**
      * 장바구니 단일 조회
-     * @param input
-     * @return
+     * @param input - 조회할 장바구니 정보에 대한 모델 객체
+     * @return 조회된 데이터
      */
     @Select("SELECT " + 
-            "cart_id, product_count, total_price, " + 
-            "member_id, product_id, " + 
-            "reg_date, edit_date " + 
+                "cart_id, " + 
+                "product_count, " + 
+                "total_price, " + 
+                "member_id, " + 
+                "product_id, " + 
+                "reg_date, " + 
+                "edit_date " + 
             "FROM cart " + 
             "WHERE cart_id = #{cartId}")
     @Results(id="cartMap", value={
@@ -79,21 +89,25 @@ public interface CartMapper {
 
     /**
      * 장바구니 다중 조회
-     * @param input
-     * @return
+     * @param input - 조회할 장바구니 정보에 대한 모델 객체
+     * @return 조회된 데이터 목록
      */
     @Select("SELECT " + 
-            "cart_id, product_count, total_price, " + 
-            "member_id, product_id, " + 
-            "reg_date, edit_date " + 
+                "cart_id, " + 
+                "product_count, " + 
+                "total_price, " + 
+                "member_id, " + 
+                "product_id, " + 
+                "reg_date, " + 
+                "edit_date " + 
             "FROM cart")
     @ResultMap("cartMap")
     public List<Cart> selectList(Cart input);
 
     /**
      * 장바구니 수 조회
-     * @param input
-     * @return
+     * @param input 
+     * @return 조회된 데이터 수
      */
     @Select("SELECT COUNT(*) FROM cart")
     public int selectCount(Cart input);
