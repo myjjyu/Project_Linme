a637f6d6cd292b7eedb84b2c5214ecda9fc0c4c1
jinju0323 <tlawlswn28@naver.com>
2024-11-19 17:32:14 +0900

    Feat: Cart 클래스의 데이터 타입 변경 #37
    - productCount, totalPrice, memberId, productId 필드를 Integer 타입으로 변경하여 NULL 값 허용
    - 참조키인 memberId와 productId를 Integer 타입으로 변경

--- linme/src/main/java/kr/project/linme/models/Cart.java
+++ linme/src/main/java/kr/project/linme/models/Cart.java
@@ -6,13 +6,13 @@ import lombok.Setter;
 
 @Data
 public class Cart {
-    private int cartId;         // 장바구니 일련번호
-    private int productCount;   // 선택한 상품의 수량
-    private int totalPrice;     // 전체 주문 금액
-    private int memberId;       // 회원 일련번호
-    private int productId;      // 상품 일련번호
-    private String regDate;     // 등록 일시
-    private String editDate;    // 변경 일시
+    private int cartId;             // 장바구니 일련번호
+    private Integer productCount;   // 선택한 상품의 수량 (Null값 허용)
+    private Integer totalPrice;     // 전체 주문 금액 (Null값 허용)
+    private Integer memberId;       // 회원 일련번호 (참조키)
+    private Integer productId;      // 상품 일련번호 (참조키)
+    private String regDate;         // 등록 일시
+    private String editDate;        // 변경 일시
 
     @Getter
     @Setter
