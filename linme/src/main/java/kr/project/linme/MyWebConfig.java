0f9951ff1b17ab504baeb1dc56a69aaf8ba804a5
jinju0323 <tlawlswn28@naver.com>
2024-11-24 23:06:20 +0900

    Feat: MyWebConfig 설정 추가 및 인터셉터 등록
    - MyInterceptor 자동 주입 및 등록
    - 업로드 파일 경로 및 URL 설정 추가
    - 특정 경로에 대한 인터셉터 제외 설정

--- linme/src/main/java/kr/project/linme/MyWebConfig.java
+++ linme/src/main/java/kr/project/linme/MyWebConfig.java
@@ -1,9 +1,11 @@
 package kr.project.linme;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import kr.project.linme.interceptors.MyInterceptor;
@@ -12,18 +14,30 @@ import kr.project.linme.interceptors.MyInterceptor;
 @SuppressWarnings("null")
 public class MyWebConfig implements WebMvcConfigurer {
 
-    // MyInteceptor 객체를 자동 주입 받는다.
-    // 이 과정에서 myInteceptor안에 @Autowired로 선언된 WebHelper 객체도 자동 주입된다.
+    // MyInercrptor 객체를 자동 주입 받는다
+    // 이 과정에서 myIntercrptor안에 @Autowired로 선언된 UtilHelper 객체도 자동 주입된다. 
     @Autowired
     private MyInterceptor myInterceptor;
+
+    /** 업로드 된 파일이 저장될 경로 (application.properties로부터 읽어옴) */
+    // --> import org.springframework.beans.factory.annotation.Value;
+    @Value("${upload.dir}")
+    private String uploadDir;
+
+    /** 업로드 된 파일이 노출될 URL 경로 (application.properties로부터 읽어옴) */
+    @Value("${upload.url}")
+    private String uploadUrl;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 직접 정의한 MyInterceptor를 Spring에 등록
         InterceptorRegistration ir = registry.addInterceptor(myInterceptor);
         // 해당 경로는 인터셉터가 가로채지 않는다.
-        ir.excludePathPatterns("/error", "/robots.txt", "/favicon.ico", "/assets/**");
-        
+        ir.excludePathPatterns("/error","/robots.txt","/favicon.ico","/assets/**");
     }
-    
+
+    /** 설정파일에 명시된 업로드 저장 경로와 URL상의 경로를 맵핑 시킴 */
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(String.format("%s/**", uploadUrl)).addResourceLocations(String.format("file://%s/", uploadDir));
+    }
 }
