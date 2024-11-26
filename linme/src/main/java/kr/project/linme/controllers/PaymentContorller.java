75422fdbf0efa7db0a1cf7ece45e0d7c71c365fa
jinju0323 <tlawlswn28@naver.com>
2024-11-26 21:37:20 +0900

    Feat: Cart, Payment UI 간단한 수정 및 경로 변경 #31 #32

--- linme/src/main/java/kr/project/linme/controllers/PaymentContorller.java
+++ linme/src/main/java/kr/project/linme/controllers/PaymentContorller.java
@@ -9,10 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PaymentContorller {
     @GetMapping("/payment/payment")
     public String payment(Model model) {
-        model.addAttribute("addressNickname", "주소 닉네임");
-        model.addAttribute("addressName", "회원이름");
-        model.addAttribute("phone", "회원 전화번호");
-        model.addAttribute("address", "회원 주소");
         
         return "payment/payment";
     }
