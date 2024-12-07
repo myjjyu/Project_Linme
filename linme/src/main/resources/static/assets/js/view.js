50632caa9dc16ff8628f88c45e9f1d5fec799ce6
Gilju Song <rlfwn@gmail.com>
2024-12-07 12:36:39 +0900

    Feat: 상세=>바구로구 이동 ajax 구현 #30

--- linme/src/main/resources/static/assets/js/view.js
+++ linme/src/main/resources/static/assets/js/view.js
@@ -18,9 +18,6 @@ var swiper2 = new Swiper(".mySwiper2", {
   },
 });
 
-
-
-
 // 상품설명, 리뷰, 상품문의 클릭시 컬러변경 이벤트
 const tabs = document.querySelectorAll(".tab-link");
 
@@ -32,8 +29,6 @@ tabs.forEach((tab) => {
     this.classList.add("on");
   });
 });
-
-
 
 //문의하기 alert창
 document.querySelector(".button").addEventListener("click", (e) => {
@@ -62,9 +57,7 @@ document.querySelector(".required-Info").addEventListener("click", (e) => {
   });
 });
 
-
-
-// 장바구니 바로구매 alert창 
+// 장바구니 alert창
 document.querySelector(".cart-btn").addEventListener("click", (e) => {
   e.preventDefault();
   console.log("isLoggedIn:", isLoggedIn);
@@ -72,7 +65,7 @@ document.querySelector(".cart-btn").addEventListener("click", (e) => {
   if (isLoggedIn) {
     // 로그인된 경우
     Swal.fire({
-      title: `상품이 장바구니에 담겼습니다.<br>장바구니로 이동하시겠습니까?`, 
+      title: `상품이 장바구니에 담겼습니다.<br>장바구니로 이동하시겠습니까?`,
       showCancelButton: true,
       cancelButtonText: "닫기",
       confirmButtonText: "이동",
@@ -80,44 +73,46 @@ document.querySelector(".cart-btn").addEventListener("click", (e) => {
         cancelButton: "cartclose-button",
         confirmButton: "cart-button",
         title: "cart-text",
-        popup: "custom-alert-popup"
+        popup: "custom-alert-popup",
       },
       reverseButtons: true, // 버튼 순서 변경
     }).then((result) => {
-
       if (result.isConfirmed) {
         // AJAX 요청을 통해 장바구니에 아이템을 추가
         var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/api/cart/cart/add', true);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    var response = JSON.parse(xhr.responseText);
-                    if (response.cart) {
-                        // 성공 시 리디렉션
-                        window.location.href = response.redirectUrl;
-                    } else {
-                        console.error('Error adding item to cart:', response.message);
-                    }
-                } else {
-                    console.error('Error adding item to cart:', xhr.statusText);
-                }
+        xhr.open("POST", "/api/cart/cart/add", true);
+        xhr.setRequestHeader(
+          "Content-Type",
+          "application/x-www-form-urlencoded"
+        );
+
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+              var response = JSON.parse(xhr.responseText);
+              if (response.cart) {
+                // 성공 시 리디렉션
+                window.location.href = response.redirectUrl;
+              } else {
+                console.error("Error adding item to cart:", response.message);
+              }
+            } else {
+              console.error("Error adding item to cart:", xhr.statusText);
             }
+          }
         };
-    
+
         // 폼 데이터를 수집하여 URL 인코딩된 문자열로 변환
         var form = document.querySelector("#cartform");
         var formData = new FormData(form);
         var data = new URLSearchParams(formData).toString();
         xhr.send(data);
-    }
+      }
     });
   } else {
     // 로그인 안 된 경우
     Swal.fire({
-      title: `로그인이 필요합니다.<br>로그인 페이지로 이동하시겠습니까?`, 
+      title: `로그인이 필요합니다.<br>로그인 페이지로 이동하시겠습니까?`,
       showCancelButton: true,
       cancelButtonText: "닫기",
       confirmButtonText: "이동",
@@ -125,23 +120,103 @@ document.querySelector(".cart-btn").addEventListener("click", (e) => {
         cancelButton: "cartclose-button",
         confirmButton: "cart-button",
         title: "cart-text",
-        popup: "custom-alert-popup"
+        popup: "custom-alert-popup",
       },
       reverseButtons: true, // 버튼 순서 변경
     }).then((result) => {
       if (result.isConfirmed) {
         document.querySelector("#cartform-not-log").submit();
-        window.location.href = "/login"; 
+        window.location.href = "/login";
       }
     });
   }
 });
 
+// 바로가기 구매 alert창
+document.querySelector(".buy-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("isLoggedIn:", isLoggedIn);
 
+  if (isLoggedIn) {
+    Swal.fire({
+      title: `바로 구매하시겠습니까?`,
+      showCancelButton: true,
+      cancelButtonText: "닫기",
+      confirmButtonText: "구매",
+      customClass: {
+        cancelButton: "cartclose-button",
+        confirmButton: "cart-button",
+        title: "cart-text",
+        popup: "custom-alert-popup",
+      },
+      reverseButtons: true, // 버튼 순서 변경
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // AJAX 요청을 통해 바로구매에 아이템을 추가
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/api/payment/payment/", true);
+        xhr.setRequestHeader(
+          "Content-Type",
+          "application/x-www-form-urlencoded"
+        );
 
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+              var response = JSON.parse(xhr.responseText);
+              if (response.cart) {
+                // 성공 시 리디렉션
+                window.location.href = response.redirectUrl;
+              } else {
+                console.error("Error adding item to cart:", response.message);
+              }
+            } else {
+              console.error("Error adding item to cart:", xhr.statusText);
+            }
+          }
+        };
 
+        // 폼 데이터를 수집하여 URL 인코딩된 문자열로 변환
+        // var form = document.querySelector("#paymentform");
+        // var formData = new FormData(form);
+        // var data = new URLSearchParams(formData).toString();
+        // xhr.send(data);
 
-// 수량 증가, 감소 버튼 
+        var form = document.querySelector("#paymentform");
+        var formData = new FormData(form);
+        var data = new URLSearchParams();
+
+        formData.forEach((value, key) => {
+          data.append(key, value);
+        });
+
+        xhr.send(data.toString());
+
+        
+      }
+    });
+  } else {
+    Swal.fire({
+      title: `로그인이 필요합니다.<br>로그인 페이지로 이동하시겠습니까?`,
+      showCancelButton: true,
+      cancelButtonText: "닫기",
+      confirmButtonText: "이동",
+      customClass: {
+        cancelButton: "cartclose-button",
+        confirmButton: "cart-button",
+        title: "cart-text",
+        popup: "custom-alert-popup",
+      },
+      reverseButtons: true, // 버튼 순서 변경
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = "/login";
+      }
+    });
+  }
+});
+
+// 수량 증가, 감소 버튼
 document.addEventListener("DOMContentLoaded", function () {
   let quantity = 1;
 
@@ -150,7 +225,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const numberDisplay = document.querySelector(".number");
   const totalPriceDisplay = document.querySelector(".total-price");
   const quantityInputs = document.querySelectorAll(".quantity-input");
-  const unitPrice = parseInt(totalPriceDisplay.getAttribute("data-unit-price")) || 0;
+  const unitPrice =
+    parseInt(totalPriceDisplay.getAttribute("data-unit-price")) || 0;
 
   console.log("Unit Price:", unitPrice);
 
@@ -159,7 +235,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const totalPrice = unitPrice * quantity;
     totalPriceDisplay.innerHTML = `${totalPrice.toLocaleString()} <span>원</span>`;
 
-    quantityInputs.forEach(input => input.value = quantity);
+    quantityInputs.forEach((input) => (input.value = quantity));
   }
 
   if (minusButton) {
@@ -191,4 +267,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
   updateTotal();
 });
-
