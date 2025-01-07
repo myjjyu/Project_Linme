0edef4e015ee517aaf7c02be0604ae1b04e7ee74
jinju0323 <tlawlswn28@naver.com>
2025-01-07 17:22:53 +0900

    Chore: Java 버전 업데이트와 Tomcat 및 war관련 설정 추가
    - application.properties : 서버 포트 80으로 변경
    - build.gradle : 'war' 플러그인 추가 및 bootWar 설정 추가
    - Main 클래스 : Tomcat 설정 추가

--- linme/src/main/java/kr/project/linme/LinmeApplication.java
+++ linme/src/main/java/kr/project/linme/LinmeApplication.java
@@ -2,16 +2,22 @@ package kr.project.linme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 // 스케쥴러 활성화
 // --> import org.springframework.scheduling.annotation.EnableScheduling; 
 @EnableScheduling
 @SpringBootApplication
-public class LinmeApplication {
+public class LinmeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinmeApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LinmeApplication.class);
+    }
 }
